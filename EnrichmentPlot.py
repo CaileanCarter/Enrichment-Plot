b8c44a4305b811fd35962024238538c45508b8fb
CaileanCarter <cailean.carter@quadram.ac.uk>
2021-03-09 17:33:28 +0000

    Minor changes to exception handling

--- EnrichmentPlot.py
+++ EnrichmentPlot.py
@@ -133,8 +133,8 @@ def Main(RoaryData, EnrichmentData, accessories=False, core=False, score=1, outp
         for gene in genes:
             try:
                 gene_presence_absence = condensedGPA.loc[gene]                        # Pull gene presence/absence from Roary data
-            except:
-                pass
+            except ValueError:
+                continue
 
             for isolateID, occur in gene_presence_absence.to_dict().items():
                 pathwaycount[isolateID] += occur
